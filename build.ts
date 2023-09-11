5e6375ec29bb3f6de359842d90d9fbe2e005ba2f
Teacup <git@teadev.xyz>
2023-09-11 13:41:05 -0400

    chore: Add cmd for build scripts, add watch script

--- build.ts
+++ build.ts
@@ -1,26 +1,43 @@
 import type { BunPlugin, BunFile } from "bun";
 import { userscriptMetadataGenerator, Metadata } from 'userscript-metadata-generator';
 import meta from "./config/metadata.cjs"
+import chokidar from "chokidar";
 
-const build = await Bun.build({
-  entrypoints: ["./src/index.ts"],
-  outdir: './dist',
-  target: "browser",
-  naming: "[dir]/openWithVRCX.user.[ext]", // default
-});
+// Check if process args contains "--watch"
+const watch = process.argv.includes("--watch");
 
-const metadataStr = userscriptMetadataGenerator(meta);
+async function doBuild() {
+  let time = Date.now()
+  const build = await Bun.build({
+    entrypoints: ["./src/index.ts"],
+    outdir: './dist',
+    target: "browser",
+    naming: "[dir]/openWithVRCX.user.[ext]", // default
+  });
+  
+  const metadataStr = userscriptMetadataGenerator(meta);
+  
+  for (const output of build.outputs) {
+    const file = Bun.file(output.path)
+    // console.log("Adding metadata to", output.path)
+  
+    // output metadata str to built file
+    let src = await output.text()
+    src = metadataStr + "\n\n" + src
+  
+    await Bun.write(file, src)
+    // console.log(await output.text()); // string
+  }
 
-for (const output of build.outputs) {
-  const file = Bun.file(output.path)
-  console.log("Adding metadata to", output.path)
+  console.log(`Built in ${Date.now() - time}ms`)
+}
 
-  // output metadata str to built file
-  let src = await output.text()
-  src = metadataStr + "\n\n" + src
+doBuild()
 
-  await Bun.write(file, src)
-  //console.log(await output.text()); // string
+if (watch) {
+  console.log("Watching for file changes...")
+  // Monitor src directory for file changes
+  chokidar.watch('./src', { ignoreInitial: true }).on('all', doBuild);
 }
 
 //console.log(userscriptMetadataGenerator(meta))
